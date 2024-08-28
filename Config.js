4b7344ab69707145d85dedb0c0b3a18992c44452
Symptome1 <83846811+Symptome1@users.noreply.github.com>
2024-08-28 01:07:16 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "6283833304947"],
-global.ownername = process.env.OWNER_NAME || "𓆩𝐓𝐎𝐆𝐄𓆪 ✇ ◤✞𝐈𝐍𝐔𝐌𝐀𝐊𝐈",
+global.ownernumber = [ process.env.OWNER_NUMBER || "2250555236730"],
+global.ownername = process.env.OWNER_NAME || "Zₑₜₛᵤ",
 global.ytname = "YT: kenzo3146"
 global.socialm = "GitHub: toge012345"
 global.location = "Africa"
@@ -11,7 +11,7 @@ global.location = "Africa"
 global.botname = process.env.BOTNAME || "𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑",
+global.stickername = process.env.STICKER || "Zₑₜₛᵤ",
 global.packname = 'Sticker By'
 global.author = process.env.AUTHOR || "𓆩𝐓𝐎𝐆𝐄𓆪 ✇ ◤✞𝐈𝐍𝐔𝐌𝐀𝐊𝐈",
 //console view/theme
@@ -22,31 +22,31 @@ global.wm = "𝐓𝐎𝐆𝐄_𝐁𝐎𝐓 𝐈𝐧𝐜 ✨"
 global.link = 'https://whatsapp.com/channel/0029VaiuD4s4IBhI0fzbv40Z'
 
 //custom prefix
-global.prefa = process.env.PREFIX || ".",
+global.prefa = process.env.PREFIX || "@",
 
 //false=disable and true=enable
 global.welcome = process.env.WELCOME || "false", //auto welcome
-global.autoRecording = false //auto recording
+global.autoRecording = true //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti212 = true //auto block +212
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 
 
 //reply messages
 global.mess = {
-    done: '💫 *Here you go, sensei!* ',
-    prem: '💫 *hello senpai, this feature is reserved for premium users only*',
-    admin: '💫 *hello senpai, this feature is reserved for admins only*',
-    botAdmin: '💫 *senpai, this feature can only be used when the bot is a group admin* ',
-    owner: '💫 *hello senpai, this feature is reserved for the owner only*',
-    group: '💫 *sensei, this feature is exclusively for groups*',
-    private: '💫 *hello senpai, this feature is exclusively for private chats*',
-    wait: '💫 *hello senpai, in process...* ',    
-    error: '💫 *hello senpai, there seems to be an error!*',
+    done: '💫 *voilà!* ',
+    prem: '💫 *ce wé est pour les premiums *',
+    admin: '💫 *ce wé est pour les admins*',
+    botAdmin: '💫 *tu dois etre admin pour utiliser cette commande* ',
+    owner: '💫 *ce wé est pour les proprios',
+    group: '💫 *ce wé est pour les groupes*',
+    private: '💫 *ce wé est pour les messages privés*',
+    wait: '💫 *en cours...* ',    
+    error: '💫 erreur!*',
 }
 
 global.thumb = fs.readFileSync('./lib/Assets/thumb.jpg')
